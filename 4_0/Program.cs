171c94057f1b7df7c7394274c17f70669617a526
Andranik <andran1401@gmail.com>
2023-03-11 23:09:49 +0400

    Четвёртое

---
+++ 4_0/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("ввидите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int evenNumber =2;
+
+if (evenNumber>1)
+{
+    while(evenNumber<=number)
+    {
+        Console.WriteLine(evenNumber+ "");
+        evenNumber=evenNumber+2;
+    }
+
+
+
+}
