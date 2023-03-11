77126953f1a4891a230c14f58b4718776017d7eb
Andranik <andran1401@gmail.com>
2023-03-11 22:53:33 +0400

    Третье

---
+++ 3_0/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("ввидите число ");
+int number =Convert.ToInt32(Console.ReadLine());
+
+int remDiv = number % 2;
+
+if (remDiv==0)
+{
+    Console.WriteLine("да");
+}
+ else
+ {
+    Console.WriteLine("Нет");
+ }
